bugfix normalizing inputs

cifar100/train_cifar100.py
@@ -217,16 +217,25 @@ class cifar100vgg:
         
     def getModel(self):
         return Model(input=self.model.input, output=self.model.layers[-2].output)
-        
 
-(x_train, y_train), (x_test, y_test) = cifar100.load_data()
-x_train = x_train.astype('float32')
+
+# The data, shuffled and split between train and test sets:
+(x_full_train, y_full_train), (x_test, y_test) = cifar100.load_data()
+x_full_train = x_full_train.astype('float32')
 x_test = x_test.astype('float32')
-x_valid = x_train[-10000:]
-y_train = keras.utils.to_categorical(y_train, 100)
-y_test = keras.utils.to_categorical(y_test, 100)
+dummy_model = cifar100vgg(train=False)
+x_full_train, x_test = dummy_model.normalize(x_full_train, x_test)
 
-for seed in np.arange(10, 100,10):
+y_full_train = keras.utils.to_categorical(y_full_train, dummy_model.num_classes)
+y_test = keras.utils.to_categorical(y_test, dummy_model.num_classes)
+
+x_train = x_full_train[:-10000]
+y_train = y_full_train[:-10000]
+x_valid = x_full_train[-10000:]
+y_valid = y_full_train[-10000:]
+
+
+for seed in np.arange(20, 100,10):
     np.random.seed(seed)
     random.seed(seed)
     model = cifar100vgg()